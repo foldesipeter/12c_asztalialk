bea50e61b83fb0fa4726a9fd847ee0a9a63f1d57
foldesipeter <foldesip24@gmail.com>
2024-09-11 09:37:19 +0200

    TobbElem

---
+++ TobbElem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11.ben nem tanított senki programozni minket");
+            Console.ReadLine();
+        }
+    }
+}
