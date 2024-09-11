0df3e557b6151c4085060dc8f270412242deb33e
foldesipeter <foldesip24@gmail.com>
2024-09-11 10:01:08 +0200

    NegyszogOOP

---
+++ NegyszogOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegyszogOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nem tanítottak ilyet nekem");
+            Console.ReadLine();
+        }
+    }
+}
