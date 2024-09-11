093964b56419d4bc540bb71e9bdf5befed49e701
foldesipeter <foldesip24@gmail.com>
2024-09-11 09:55:52 +0200

    Matrix

---
+++ Matrix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nem tanultam ilyet");
+            Console.ReadLine();
+        }
+    }
+}
