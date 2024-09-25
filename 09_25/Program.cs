03dc4e6045059ce15e4d9c6bb9ec5ac32785655e
foldesipeter <foldesip24@gmail.com>
2024-09-25 08:52:11 +0200

    asd

---
+++ 09_25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*asd*/
+
+namespace _09_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
--- TobbElem/Program.cs
+++ TobbElem/Program.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Írjunk olyan programot, amely addig kér
+be egész számokat a billentyűzetről, amíg azok összege meg nem haladja a 100-at. A
+beolvasás végén írjuk ki azt, hogy a bekért számok közül hány volt páros, és hány volt
+páratlan.*/
+
 namespace TobbElem
 {
+    class TobbElemOsztaly
+    {
+        // Osztályváltozók
+        private int szam;
+        private bool isParos;
+        // Konstuktor
+        public TobbElemOsztaly()
+        {
+            szam = 0;
+            isParos = false;
+        }
+
+        public void GetSzam(int p)
+        {
+            szam = p;
+        }
+        public void SetSzamTipus()
+        {
+            isParos = szam % 2 == 0;
+        }
+        public bool outSzamTipus()
+        {
+            return isParos;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("11.ben nem tanított senki programozni");
-            Console.ReadLine();
+            string valasz = string.Empty;
+            // Példányosíjuk az osztályt, másnéven objektumot hozunk létre az osztályból
+            TobbElemOsztaly a = new TobbElemOsztaly();
+
+            // Szám megadása
+            a.GetSzam(15);
+            a.SetSzamTipus();
+            valasz = a.outSzamTipus() ? "páros" : "páratlan";
+            Console.WriteLine($"A szám típusa: {valasz}");
+            Console.ReadKey();
+
         }
     }
 }
