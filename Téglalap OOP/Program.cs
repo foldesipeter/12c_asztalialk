31419a1d94ecb92347fabe35e2aa2cbf3bece5d8
foldesipeter <foldesip24@gmail.com>
2024-10-09 10:25:36 +0200

    teglalap

--- Téglalap OOP/Program.cs
+++ Téglalap OOP/Program.cs
@@ -6,46 +6,42 @@ using System.Threading.Tasks;
 
 namespace Téglalap_OOP
 {
-    class Program
+    class Teglalapszamitasok
     {
-        static void Main(string[] args)
-        {class Teglalapszamitasok
+        private double aoldal,
+            boldal,
+            terulet,
+            kerulet;
+
+        public Teglalapszamitasok()
         {
-            private double aoldal,
-                boldal,
-                terulet,
-                kerulet;
-
-            public Teglalapszamitasok()
-            {
-
-            }
-            public Teglalapszamitasok(double a, double b)
-            {
-                this.aoldal = a;
-                this.boldal = b;
-            }
-            public void ReadAoldal(double aoldal)
-            {
-                this.aoldal = aoldal;
-            }
-            public void ReadBoldal(double boldal)
-            {
-                this.boldal = boldal;
-            }
-            public void TeruletSzam()
-            {
-                this.terulet = this.aoldal * this.boldal;
-            }
-            public void KeruletSzam()
-            {
-                this.kerulet = (this.aoldal + this.boldal) * 2;
-            }
-            public double GetKerulet() { return kerulet; }
-            public double GetTerulet() { return terulet; }
-
 
         }
-    }
+        public Teglalapszamitasok(double a, double b)
+        {
+            this.aoldal = a;
+            this.boldal = b;
+        }
+        public void ReadAoldal(double aoldal)
+        {
+            this.aoldal = aoldal;
+        }
+        public void ReadBoldal(double boldal)
+        {
+            this.boldal = boldal;
+        }
+        public void TeruletSzam()
+        {
+            this.terulet = this.aoldal * this.boldal;
+        }
+        public void KeruletSzam()
+        {
+            this.kerulet = (this.aoldal + this.boldal) * 2;
+        }
+        public double GetKerulet() { return kerulet; }
+        public double GetTerulet() { return terulet; }
+
+
     }
 }
+
