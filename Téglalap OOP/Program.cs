295575c09832a9594a115339bb6ab79c7e00a91c
foldesipeter <foldesip24@gmail.com>
2024-10-09 10:10:31 +0200

    tegla

---
+++ Téglalap OOP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Téglalap_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {class Teglalapszamitasok
+        {
+            private double aoldal,
+                boldal,
+                terulet,
+                kerulet;
+
+            public Teglalapszamitasok()
+            {
+
+            }
+            public Teglalapszamitasok(double a, double b)
+            {
+                this.aoldal = a;
+                this.boldal = b;
+            }
+            public void ReadAoldal(double aoldal)
+            {
+                this.aoldal = aoldal;
+            }
+            public void ReadBoldal(double boldal)
+            {
+                this.boldal = boldal;
+            }
+            public void TeruletSzam()
+            {
+                this.terulet = this.aoldal * this.boldal;
+            }
+            public void KeruletSzam()
+            {
+                this.kerulet = (this.aoldal + this.boldal) * 2;
+            }
+            public double GetKerulet() { return kerulet; }
+            public double GetTerulet() { return terulet; }
+
+
+        }
+    }
+    }
+}
